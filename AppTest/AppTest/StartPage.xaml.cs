5b1e73b6aa602c359d7e1a914b0a17430932b537
Lydia Vialson <television.orion@gmail.com>
2022-11-23 19:26:44 +0300

    v

---
+++ AppTest/AppTest/StartPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+            Label label = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                Text = ""
+            };
+            Button btn = new Button
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Text = "Click on the button"
+            };
+            
+            Content = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Children = {btn, label}
+            };
+
+            btn.Clicked += delegate
+            {
+                if (label.Text == "Hello world!") 
+                    label.Text = "See you again!";
+                else 
+                    label.Text = "Hello world!";
+            };
+
+        }
+
+    }
+}
